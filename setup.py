c9289b62728fd4ae4553238919eae974773d9f1d
bpereira <bpereira@wyplay.com>
2014-11-25 13:51:56 +0000

    fix xtarget-update-latest to use the right template ebuild

--- setup.py
+++ setup.py
@@ -106,7 +106,7 @@ def find_packages(dir):
 
 setup(
     name = "xtarget",
-    version = "2.0.19",
+    version = "2.0.20",
     description = "Xtarget tools for genbox",
     author = "Wyplay",
     author_email = "noreply@wyplay.com",
